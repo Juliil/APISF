3bf532411708541b74df27708839d6d1991a9e46
Julio Rezende <juliorezende@bemol.com.br>
2020-07-23 15:06:23 -0400

    Fix: code coments

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const multer = require("multer");
 const serveIndex = require("serve-index")
 
+//definição da view
 app.set('view engine', 'ejs');
 
+//configuração do multer para armazenamento
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
         cb(null,"uploads/");
@@ -15,13 +17,16 @@ const storage = multer.diskStorage({
 })
 const upload = multer({storage})
 
+//endpoints de consumo
 app.use("/uploads", express.static(__dirname + '/uploads'));
 app.use('/uploads', serveIndex(__dirname + '/uploads'));
 
+//Requisição da view
 app.get("/", (req, res) => {
     res.render("index");
 })
 
+//Envio de arquivos
 app.post("/upload", upload.single("file"), (req, res) =>{
     res.send("Arquivo Enviado com sucesso!");
 })
